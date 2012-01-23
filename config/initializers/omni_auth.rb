50d70b2895eb9a68dfd91fcddac24f76e6e2283c
Michael Economy <michael@michaeleconomy.com>
2012-01-22 22:24:01 -0800

    omniauth uncommented

--- config/initializers/omni_auth.rb
+++ config/initializers/omni_auth.rb
@@ -1,13 +1,13 @@
-# NETFLIX_KEY = Secret['netflix_key']
-# NETFLIX_SECRET = Secret['netflix_secret']
-# 
-# FACEBOOK_KEY = Secret['facebook_key']
-# FACEBOOK_SECRET = Secret['facebook_secret']
-# 
-# if Rails.env.development?
-#   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-# end
-# Rails.application.config.after_initialize.middleware.use OmniAuth::Builder do
-#   provider :netflix, NETFLIX_KEY, NETFLIX_SECRET
-#   provider :facebook, FACEBOOK_KEY, FACEBOOK_SECRET, :scope => "", :client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}
-# end
+NETFLIX_KEY = Secret['netflix_key']
+NETFLIX_SECRET = Secret['netflix_secret']
+
+FACEBOOK_KEY = Secret['facebook_key']
+FACEBOOK_SECRET = Secret['facebook_secret']
+
+if Rails.env.development?
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+end
+Rails.application.config.after_initialize.middleware.use OmniAuth::Builder do
+  provider :netflix, NETFLIX_KEY, NETFLIX_SECRET
+  provider :facebook, FACEBOOK_KEY, FACEBOOK_SECRET, :scope => "", :client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}
+end
